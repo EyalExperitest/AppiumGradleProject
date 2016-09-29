73d37c2afaaf9bb49b85fc2fd9859cb835134be5
eyalExperitest <eyal@experitest.com>
2016-09-29 17:13:37 +0300

    Changing UDID

--- src/test/java/AppiumOnManualTest.java
+++ src/test/java/AppiumOnManualTest.java
@@ -15,14 +15,13 @@ import java.net.MalformedURLException;
  */
 public class AppiumOnManualTest {
     public static final String APPIUM_IP = "127.0.0.1";//"192.168.1.14";
-    public static final String ID = "3230d293cf7611a3";
+    public static final String ID = "HT51HWV00455";
     public static final int APPIUM_PORT = 4723;
     public static final int BOOT_STRAP_PORT = 4751;
     public static final String CLOUD_IP = "192.168.4.63";
     public static final String CLOUD_PORT = "8090";
     public static final String PROJECT_NAME = "Default";
     public static final String USER_NAME = "eyal";
-    public static final String PASSWORD = "Experitest2012";
     private STMProcess stmProcess;
     private STRDProcess strdProcess;
 
@@ -33,7 +32,7 @@ public class AppiumOnManualTest {
         /*
         stmProcess= new STMProcess(false);
         stmProcess.waitForLaunch();*/
-        strdProcess =new STRDProcess(true,ID, CLOUD_IP, CLOUD_PORT, PROJECT_NAME, USER_NAME, PASSWORD);
+        strdProcess =new STRDProcess(true,ID, CLOUD_IP, CLOUD_PORT, PROJECT_NAME, USER_NAME, "Experitest2012");
         Thread.sleep(5000);
         //int reserve = stmProcess.reserve(ID);
         // System.out.println("Reserve Device with id :"+ID+" Result :"+ reserve);
